19dbbb176d66cb2181fc752e29ff108ac2f0ece5
Anthony Smith <anthony@sticksnleaves.com>
2017-03-26 18:25:32 -0400

    Added comments to mock adapter

--- lib/me/adapters/mock.ex
+++ lib/me/adapters/mock.ex
@@ -1,12 +1,19 @@
 defmodule Me.Adapter.Mock do
+  @moduledoc """
+  Mock adapter used for testing.
+  """
+
   @behaviour Me.Adapter
 
+  @doc false
   def init(opts), do: opts
 
+  @doc false
   def verify_password(_password, _secured_password, _opts) do
     true
   end
 
+  @doc false
   def secure_password(_password, _opts) do
     "MOCKPASSWORD"
   end
