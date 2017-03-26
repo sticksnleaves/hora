4166711dd31525742eab9cad28dbbb1fa90a0fc5
Anthony Smith <anthony@sticksnleaves.com>
2017-03-26 18:35:50 -0400

    Namespace config by module

--- lib/me/util.ex
+++ lib/me/util.ex
@@ -5,17 +5,17 @@ defmodule Me.Util do
   Get config value from either the provided `opts` or `Application` config. If
   no value is found `nil` will be returned.
   """
-  def get_config(key, opts) do
+  def get_config(module, key, opts) do
     opts[key]
-    || Application.get_env(:me, key)
+    || Application.get_env(:me, module)[key]
   end
 
   @doc """
   Get config value from either the provided `opts` or `Application` config. If
   not value is found an `ArgumentError` will be raised.
   """
-  def get_config!(key, opts) do
-    get_config(key, opts)
+  def get_config!(module, key, opts) do
+    get_config(module, key, opts)
     || raise ArgumentError
   end
 end
