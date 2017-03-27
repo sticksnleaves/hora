48d54e2bc808987d556c8b7cf8538e0f29b8a588
Anthony Smith <anthony@sticksnleaves.com>
2017-03-27 10:51:14 -0400

    Decorate a module with password management functions

--- lib/me.ex
+++ lib/me.ex
@@ -1,18 +1,41 @@
 defmodule Me do
-  @moduledoc """
-  Documentation for Me.
-  """
+  @type opts :: map | list
 
-  @doc """
-  Hello world.
+  @doc false
+  def init(opts) do
+    [
+      adapter: Me.Util.get_config!(Me, :adapter, opts),
+      adapter_options: Me.Util.get_config(Me, :adapter_options, opts) || [],
+      secure_password_field_name: Me.Util.get_config(Me, :secure_password_field_name, opts) || :password_digest
+    ]
+  end
 
-  ## Examples
+  defmacro __using__(opts) do
+    options = Me.init(opts)
 
-      iex> Me.hello
-      :world
+    quote do
+      def authenticate(%{} = schema, password) do
+        secure_password_field_name = unquote(options)[:secure_password_field_name]
+        secured_password = schema[secure_password_field_name]
 
-  """
-  def hello do
-    :world
+        verify_password(secured_password, password)
+      end
+
+      def secure_password(password) do
+        adapter = unquote(options)[:adapter]
+        adapter_options = unquote(options)[:adapter_options]
+
+        adapter
+          .secure_password(password, adapter.init(adapter_options))
+      end
+
+      def verify_password(secured_password, password) do
+        adapter = unquote(options)[:adapter]
+        adapter_options = unquote(options)[:adapter_options]
+
+        adapter
+          .verify_password(password, secured_password, adapter.init(adapter_options))
+      end
+    end
   end
 end
--- test/me_test.exs
+++ test/me_test.exs
@@ -1,8 +1,25 @@
 defmodule MeTest do
   use ExUnit.Case
-  doctest Me
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmodule TestModule do
+    use Me, adapter: Me.Adapter.Mock
+  end
+
+  describe "authenticate/2" do
+    test "checks against the value in secure_password_field_name" do
+      assert TestModule.authenticate(%{password_digest: "b"}, "b") == true
+    end
+  end
+
+  describe "verify_password/2" do
+    test "calls adapter" do
+      assert TestModule.verify_password("a", "a") == true
+    end
+  end
+
+  describe "secure_password/1" do
+    test "calls adapter" do
+      assert TestModule.secure_password("a") == "MOCKPASSWORD"
+    end
   end
 end
